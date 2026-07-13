89020c80330945bd76397e83ac4d3eba7cf0d721
Nyukaa <annashitikova@gmail.com>
2026-07-13 12:47:01 +0300

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, World!");
+//Раньше программа выглядела так:
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.WriteLine("Hello");
+//     }
+// }
